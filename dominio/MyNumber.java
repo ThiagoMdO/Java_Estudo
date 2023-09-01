5c5cc87b7859677f3e7c0b6a612776e4a6001aa4
ThiagoMdO <128644651+ThiagoMdO@users.noreply.github.com>
2023-09-01 10:39:19 -0300

    printNumberTriangle

--- dominio/MyNumber.java
+++ dominio/MyNumber.java
@@ -2,31 +2,57 @@ package academy.devdojo.maratonajava.javacore.CompassUol.dominio;
 
 public class MyNumber {
     private int number;
-    public MyNumber(int number){
+
+    public MyNumber(int number) {
         this.number = number;
     }
 
-    public boolean isPrime(){
-        for(int i = 2; i <= this.number - 1; i++){
-            if(this.number % i == 0) {
+    public boolean isPrime() {
+        for (int i = 2; i <= this.number - 1; i++) {
+            if (this.number % i == 0) {
                 return false;
             }
         }
         return true;
     }
-    public int sumUptoN(){
+
+    public int sumUptoN() {
         int sum = 0;
-        for(int i = 1; i <= this.number; i++){
+        for (int i = 1; i <= this.number; i++) {
 
             sum = sum + i;
-            if(i==1){
+            if (i == 1) {
                 System.out.print(sum);
-            }else{
-                System.out.print(" + "+sum);
+            } else {
+                System.out.print(" + " + sum);
             }
 
 
         }
         return sum;
+    }
+
+    public int sumOfDivisors() {
+        int sum = 0;
+
+        for (int i = 2; i < this.number - 1; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+            return 0;
+
+        }
+        return sum;
+    }
+
+    public void printNumberTriangle() {
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+
+        }
+
     }
 }
--- teste/MyNumberRunner.java
+++ teste/MyNumberRunner.java
@@ -2,12 +2,16 @@ package academy.devdojo.maratonajava.javacore.CompassUol.teste;
 import academy.devdojo.maratonajava.javacore.CompassUol.dominio.MyNumber;
 public class MyNumberRunner {
     public static void main(String[] args){
-        MyNumber number = new MyNumber(9);
+        MyNumber number = new MyNumber(17);
         boolean isPrime = number.isPrime();
         System.out.println("Is prime? "+isPrime);
 
         int sumUptoN = number.sumUptoN();
         System.out.println("sumUptoN "+ sumUptoN);
+
+        System.out.println(number.sumOfDivisors());
+
+        number.printNumberTriangle();
     }
 
 }
